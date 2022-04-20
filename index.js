f6a8b1e58f6427d2f0d9e39bb80ab59593952871
Abdul Aziz <azizic98@gmail.com>
2022-04-20 15:16:33 +0530

    Final Destination Added

---
+++ index.js
@@ -0,0 +1,5 @@
+var n = prompt("Enter your name:");
+var dt = new Date();
+document.getElementById("date-time").innerHTML = dt;
+
+document.getElementById("welcome").innerHTML = n + " has reached here at : ";
